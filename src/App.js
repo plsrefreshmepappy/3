a673f437274f0033fa5c33443fa270de9731fa6e
plsrefreshmepappy <123778536+plsrefreshmepappy@users.noreply.github.com>
2023-02-09 20:11:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -370,7 +370,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-              {data.totalSupply-5} / {CONFIG.MAX_SUPPLY}
+              {data.totalSupply} / {CONFIG.MAX_SUPPLY}
             </s.TextTitle>
 
             <span
@@ -381,7 +381,7 @@ function App() {
 
             </span>
             <s.SpacerSmall />
-            {Number(data.totalSupply-5) >= CONFIG.MAX_SUPPLY ? (
+            {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
               <>
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
